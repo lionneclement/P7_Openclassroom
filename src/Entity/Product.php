b2b8b9cedcaac4ae7e7c062aaa4ddadd4177fdf2
clement <lionneclement@gmail.com>
2020-04-26 15:01:28 +0200

    add serializer groups && use hateoas

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,9 +3,22 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Hateoas\Configuration\Annotation as Hateoas;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * 
+ * @Hateoas\Relation("self",
+ *      href = @Hateoas\Route(
+ *          "api_find_one_product",
+ *          parameters = {
+ *              "id" = "expr(object.getId())"
+ *          },
+ *      absolute = true
+ *      ),
+ *      exclusion = @Hateoas\Exclusion(groups={"all"})
+ * )
  */
 class Product
 {
@@ -13,21 +26,25 @@ class Product
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Serializer\Groups({"all"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Serializer\Groups({"all","one"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="integer")
+     * @Serializer\Groups({"all","one"})
      */
     private $price;
 
     /**
      * @ORM\Column(type="text")
+     * @Serializer\Groups({"all","one"})
      */
     private $description;
 
