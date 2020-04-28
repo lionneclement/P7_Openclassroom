122ea7cde6d5bc2eb2e5d5f1eeb6676ed6d9d7c2
clement <lionneclement@gmail.com>
2020-04-28 17:30:25 +0200

    install Knp Paginator

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
